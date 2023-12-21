f062ef812d3b13ef1901cc30291ee7fac3ba66d9
Denis <den.grigorev.2002@list.ru>
2023-12-21 21:59:20 +0300

    add lesson:task4

---
+++ Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 123;
+int b = 34;
+int c = 432;
+int d = 44;
+int e = 67;
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
