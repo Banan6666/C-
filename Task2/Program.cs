c8861fc41896b77e788d16a64f2001aca6f6efb0
Denis <den.grigorev.2002@list.ru>
2023-12-21 21:27:11 +0300

    add lesson:task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,6 +6,11 @@ Console.WriteLine(sum);
 long l1 = 6;
 long sum2 = l1 + i1;
 double d1 = 5.5;
-double sum3 = l1+ d1;
+double sum3 = l1 + d1;
 Console.WriteLine(sum2);
 Console.WriteLine(sum3);
+
+int a = 33;
+d1 = 6;
+double quotient = a / d1;
+Console.WriteLine(quotient);
